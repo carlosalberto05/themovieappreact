c19a708fcfd6bdcea971883e82e6bec813918505
Carlos Alberto Lira <47789867+carlosalberto05@users.noreply.github.com>
2023-07-11 14:31:57 -0600

    avance maquetacion

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,12 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 
 const Router = () => {
+  const isLogged = false;
+
   return (
-    <div>
-      {/* <Login /> */}
-      <Home />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={isLogged ? <Navigate to="/inicio" /> : <Login />}
+      />
+      <Route path="/inicio" element={<Home />} />
+    </Routes>
   );
 };
 
